3049bb52c8ac26e424fe03ed0ecb129be9ec537f
nyanyacyan <nyanyacyan@gmail.com>
2025-05-16 03:21:00 +0900

    エラークラス、Warningクラスを定義

--- installer/src/method/base/utils/error_hander.py
+++ installer/src/method/base/utils/error_hander.py
@@ -13,7 +13,7 @@ from method.base.utils.popup import Popup
 # **********************************************************************************
 
 
-class CriticalError:
+class CriticalHandler:
     def __init__(self):
 
         # logger
@@ -33,3 +33,52 @@ class CriticalError:
         sys.exit(1)
 
     #!###################################################################################
+
+# **********************************************************************************
+
+
+class ErrorHandler:
+    def __init__(self):
+
+        # logger
+        self.getLogger = Logger()
+        self.logger = self.getLogger.getLogger()
+
+        # インスタンス
+        self.popup = Popup()
+
+    #!###################################################################################
+    # criticalエラーの際にポップアップを表示してsystem.exitする
+
+    def popup_process(self, err_title: str, err_msg: str, e: Exception):
+        error_comment = f"{err_msg}\n{e}"
+        self.logger.error(error_comment)
+        self.popup.popupCommentOnly(popupTitle=err_title, comment=error_comment)
+        sys.exit(1)
+
+    #!###################################################################################
+
+# **********************************************************************************
+
+
+class WarningHandler:
+    def __init__(self):
+
+        # logger
+        self.getLogger = Logger()
+        self.logger = self.getLogger.getLogger()
+
+        # インスタンス
+        self.popup = Popup()
+
+    #!###################################################################################
+    # criticalエラーの際にポップアップを表示してsystem.exitする
+
+    def popup_process(self, err_title: str, err_msg: str, e: Exception):
+        error_comment = f"{err_msg}\n{e}"
+        self.logger.error(error_comment)
+        self.popup.popupCommentOnly(popupTitle=err_title, comment=error_comment)
+        sys.exit(1)
+
+    #!###################################################################################
+
